075c96196bfa2a1fc6db4a7b1afbc953d72cfc55
dfelcey <dfelcey@dfelcey-ltm3.lan>
2019-02-24 19:26:50 +0000

    Saved transformation

--- src/main/resources/match.groovy
+++ src/main/resources/match.groovy
@@ -1,9 +1,11 @@
 @Grab('org.apache.commons:commons-lang3:3.5')
 import static org.apache.commons.lang3.StringUtils.getJaroWinklerDistance
 
+def result = ""
+
 // Check inputs
 if(text == null || text.trim().isEmpty() || keywords == null || keywords.trim().isEmpty()){
-  println "No match"
+  result = "No match"
 } else {
   def words = keywords.toLowerCase().split(',')
   def textList = text.toLowerCase().split().toList().unique()
@@ -15,5 +17,7 @@ if(text == null || text.trim().isEmpty() || keywords == null || keywords.trim().
     }.sort { -it.get(1) }.first().get(1)
   }
 
-  println total / words.size() > 0.90 ? "Match" : "No match"
+  result = total / words.size() > 0.90 ? "Match" : "No match"
 }
+
+payload = "{ \"result\": \"" + result + "\" }"
